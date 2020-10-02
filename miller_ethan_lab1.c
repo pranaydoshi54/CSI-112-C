07539945646dd9124908fd939a3580f95e92ebee
pranaydoshi54 <71939040+pranaydoshi54@users.noreply.github.com>
2020-10-02 11:34:10 +0530

    Update miller_ethan_lab1.c
    Short form will make it easier and faster for the programmer.
    Thank You.

--- miller_ethan_lab1.c
+++ miller_ethan_lab1.c
@@ -3,33 +3,34 @@
  * Lab 1, CSCI 112
  * 1/23/17
  */
- #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
  int
  main(void)
  {
     /* declare required variables */
-    double celsius, fahrenheit;
+    double C, F;
 
     /* get the temperature in Celsius */
     printf("Enter a temperature in degrees Celsius: ");
-    scanf("%lf", &celsius);
+    scanf("%lf", &C);
 	
     /* convert to Fahrenheit */
-    fahrenheit = (1.8 * celsius) + 32;	
+    F = (1.8 * C) + 32;	
 
     /* print out the temp in Fahrenheit */
-    printf("That is %.2lf Fahrenheit \n", fahrenheit);
+    printf("That is %.2lf Fahrenheit \n", F;
 
     /* get the temperature in Fahrenheit */
     printf("Enter a temperature in degrees Fahrenheit: ");
-    scanf("%lf", &fahrenheit);
+    scanf("%lf", &F);
 
     /* convert to celsius */
-    celsius = ((fahrenheit - 32) * (5.0/9.0));
+    C = ((F - 32) * (5.0/9.0));
 
     /* print out the temp in Celsius */
-    printf("That is %.2lf Celsius \n", celsius);
+    printf("That is %.2lf Celsius \n", C);
 
     /* exit with no errors */
     return(0);
